135a0ef259c74b9f09183f40beb69195abab2749
Salil Naik <salil.naik27@gmail.com>
2021-11-19 10:36:54 +0530

    add: build setup

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Dell\\projects\\polygon\\polygonmesh\\src\\css\\custom.css"),
+];
